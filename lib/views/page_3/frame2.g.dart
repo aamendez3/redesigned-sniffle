00a69bd3ba3787210c61d7e81b53d4b0950c2df2
Parabeac-Nest <admin@parabeac.com>
2022-08-04 21:08:18 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/views/page_3/frame2.g.dart
@@ -0,0 +1,31 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+
+class Frame2 extends StatefulWidget {
+  const Frame2({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _Frame2 createState() => _Frame2();
+}
+
+class _Frame2 extends State<Frame2> {
+  _Frame2();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
